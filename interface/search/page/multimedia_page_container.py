31f73459c4734fa4e686194ff63049d097d4003a
Asigan <asiganthesunk@gmail.com>
2021-05-01 21:19:18 +0200

    remove methods from MultimediaInformationContainer

--- interface/search/page/multimedia_page_container.py
+++ interface/search/page/multimedia_page_container.py
@@ -22,37 +22,6 @@ class MultimediaPageContainer(Frame):
         self.pack(side=TOP, fill=X)
         self.root_container = root_container
 
-    @staticmethod
-    def on_select_entry(magnet_data):
-        name, seed, leech, size = magnet_data
-        print(f'clicked: {name}, {seed}, {leech}, {size}')
-        return name, seed, leech, size
-
-    def add_entry(self, magnet_data):
-        self.magnet_list_box.insert_row([*magnet_data])
-
-    @staticmethod
-    def init_metro_text_label(root_container, text):
-        for _text in text:
-            metro_text_label = Label(root_container, text=_text)
-            metro_text_label.pack(anchor=NW)
-
-    @staticmethod
-    def init_directors_quote_frame(container_body, directors=''):
-        metro_quote_container = MetroQuoteContainer(container_body, text=f'Director: {directors}')
-        metro_quote_container.pack(anchor=NW)
-
-    @staticmethod
-    def init_actors_quote_frame(container_frame, actors=''):
-        metro_actors_quote_frame = MetroQuoteContainer(container_frame, text=f'Actors: {actors}')
-        metro_actors_quote_frame.pack(anchor=NW)
-
-    @staticmethod
-    def init_title_label_frame(container_frame, title=''):
-        metro_labelframe_container = MetroLabelContainer(container_frame, title=title.title())
-        metro_labelframe_container.pack(fill=BOTH, side=LEFT, padx=2, pady=2, expand=True)
-        return metro_labelframe_container
-
     def init_new_notebook_tab(self, background_color):
         self.configure(bg=background_color)
 
@@ -87,3 +56,9 @@ class MultimediaPageContainer(Frame):
         MultimediaDownloadContainer(self)
 
         self.root_container.add(self, text=background_color)
+
+    @staticmethod
+    def on_select_entry(magnet_data):
+        name, seed, leech, size = magnet_data
+        print(f'clicked: {name}, {seed}, {leech}, {size}')
+        return name, seed, leech, size
